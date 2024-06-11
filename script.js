10b00080a1069af2d761ac4580383200a2a297eb
Jean Lucas <j3anlucas010@gmail.com>
2024-06-11 09:11:47 -0300

    Numeros Pares E impar

---
+++ script.js
@@ -0,0 +1,9 @@
+function soma(numero) {
+  if (numero % 2 === 0) return "Par";
+  else return "Impar";
+}
+console.log(soma(1));
+
+const numero=2
+const ParImpar=numero%2==0?'Par':'Impar'
+console.log(ParImpar)
